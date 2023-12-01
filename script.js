bb5e56bf8931629ba34238c48b21a7b609f86ff4
Airstrike213 <art3aga04@gmail.com>
2023-12-01 20:34:12 +0100

    Final Sprint
    Changes some sizes of fonts. Aded Responsive Footer with lists. Added Changeable text with each slide, updated card styles. Added the Button to show the overlay. Not a Hover anymore. Changed the Sizes of all logos

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   });
 });
 
+// Initialize header slide index
 let headerSlideIndex = 0;
 let headerSlideInterval;
 
 function changeHeaderSlide(n) {
   const headerSlides = document.getElementsByClassName("header-slide");
+  const heading = document.querySelector('.daw-display-middle h1 span');
 
   // Hide all slides
   for (let i = 0; i < headerSlides.length; i++) {
@@ -28,13 +30,24 @@ function changeHeaderSlide(n) {
 
   // Show the current slide
   headerSlides[headerSlideIndex].style.display = "block";
+
+  // Update heading content based on the current slide
+  if (headerSlideIndex === 0) {
+    heading.innerHTML = '<b>Gartenpflege</b>';
+  } else if (headerSlideIndex === 1) {
+    heading.innerHTML = '<b>Baumpflege</b>';
+  } else if (headerSlideIndex === 2) {
+    heading.innerHTML = '<b>Pflastern</b>';
+  }
+
+  console.log("Slide index:", headerSlideIndex);
 }
 
 function startAutomaticSlide() {
   headerSlideInterval = setInterval(function () {
     console.log("Automatic slideshow triggered.");
     changeHeaderSlide(1);
-  }, 7000); // Change slide every 2 seconds (adjust as needed, the three last 000 should stay, everything before that says the seconds.)
+  }, 7000); // Change slide every 7 seconds (adjust as needed)
 }
 
 // Start automatic slideshow
@@ -62,3 +75,29 @@ window.onload = function () {
     startAutomaticSlide(); // Start automatic slideshow again
   });
 };
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for the DOM to be fully loaded
+
+  // Get the button and the overlay
+  const toggleButton = document.getElementById("toggleOverlay");
+  const overlay = document.querySelector(".new-text-overlay");
+
+  // Add a click event listener to the button
+  toggleButton.addEventListener("click", function () {
+    // Toggle the 'show' class on the overlay to display or hide it
+    overlay.classList.toggle("show");
+  });
+});
+function toggleOverlay(button) {
+  // Find the parent container of the button
+  const container = button.closest('.image-container');
+
+  // Find the overlay within the same container
+  const overlay = container.querySelector('.new-text-overlay');
+
+  // Toggle the visibility of the overlay
+  overlay.classList.toggle('show');
+}
